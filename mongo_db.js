400607e6f1e9f466e0c10a0466210f5be4f3884c
Perry <olsson.perry@gmail.com>
2020-09-28 20:31:09 -0700

    db script

--- mongo_db.js
+++ mongo_db.js
@@ -1,42 +1,64 @@
-const mongoose = require('mongoose')
-const config = require('./utils/config')
-const logger = require('./utils/logger')
+const mongoose = require('mongoose');
+const config = require('./utils/config');
+const logger = require('./utils/logger');
+const Blog = require('./models/blog');
+const User = require('./models/user');
+const helper = require('./tests/test_helper');
+const bcrypt = require('bcrypt');
 
-const args = process.argv.length
+const args = process.argv.length;
 
-if (args !== 6 && args !== 2) {
-  throw new Error('missing arguments')
-}
-const mongoUrl = config.MONGODB_URI
+if (args > 3) throw new Error('accepts only one argument');
 
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+const mongoUrl = config.MONGODB_URI;
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+mongoose
+  .connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('connected'))
+  .catch(() => console.log('not connected'));
+const main = async () => {
+  if (process.argv[3] === 'reset') {
+    await Blog.deleteMany({});
+    await User.deleteMany({});
 
-const Blog = mongoose.model('Blog', blogSchema)
+    for (let user of helper.initialUsers) {
+      const passwordHash = await bcrypt.hash(user.password, 10);
 
-const blog = new Blog({
-  title: process.argv[2],
-  author: process.argv[3],
-  url: process.argv[4],
-  likes: process.argv[5]
-})
+      delete user.password;
+      let newUser = new User({
+        ...user,
+        passwordHash,
+        likes: new Map(),
+      });
 
-args === 6 ? blog
-  .save()
-  .then(result => {
-    logger.info('saved: ', result)
-    mongoose.connection.close()
+      await newUser.save();
+    }
+
+    const loginTestUser = async () => {
+      const response = await api
+        .post('/api/login')
+        .send({ username: 'testUsername', password: 'testPassword' });
+
+      return response.body;
+    };
+
+    await api.post('/api/users').send({
+      username: 'testUsername',
+      name: 'testName',
+      password: 'testPassword',
+    });
+
+    let userToken = await loginTestUser();
+    for (let blog of helper.initialBlogs) {
+      await api
+        .post('/api/blogs')
+        .set('Authorization', `bearer ${userToken.token}`)
+        .send(blog);
+    }
+  }
+};
+main()
+  .then(() => {
+    mongoose.connection.close();
   })
-  :
-  Blog.find({}).then(result => {
-    logger.info(result)
-    mongoose.connection.close()
-  })
-
-
+  .catch(e => console.log(e.message));
